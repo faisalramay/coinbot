09fa1721a62d08c057d12c6a9f921a07922a9c9b
faisalramay <faisalramay@gmail.com>
2017-06-22 13:14:54 +0800

    add last_update to index.pug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var moment = require('moment');
+var Redis = require('ioredis');
+var config = require('../config');
+
 var router = express.Router();
+var redis = new Redis(config.redis.port);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	
+	var data = {};
+	
+	redis.get('poloniex-last-update', function(err, last_update) {
+		console.log('last_update: ' + last_update);
+		res.render('index', { 'last_update': moment(last_update, 'x').fromNow() });
+	}); 
 });
 
 module.exports = router;
